9aab5dec09fe54f3bbfe068ece0c7a03a8008ea6
Kevin Wang <kwang30@stuy.edu>
2022-09-21 21:44:34 -0400

    list1 copied over

--- 03_py/warmup1/sum_double.py
+++ 03_py/warmup1/sum_double.py
@@ -2,13 +2,4 @@ def sum_double(a, b):
   sum = a + b
   if (a == b ):
     return sum * 2
-  return sum #don't need else before returning
-
-print(sum_double(1, 2)) #→ 3
-print(sum_double(3, 2)) #→ 5
-print(sum_double(2, 2)) #→ 8
-print(sum_double(-1, 0)) #→ -1
-print(sum_double(3, 3)) #→ 12
-print(sum_double(0, 0)) #→ 0
-print(sum_double(0, 1)) #→ 1
-print(sum_double(3, 4)) #→ 7
+  return sum
