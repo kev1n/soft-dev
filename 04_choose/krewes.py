d45ef50898fa7c3c7c816c449c79eb51fadcfb73
Kevin Wang <kwang30@stuy.edu>
2022-09-22 13:51:47 -0400

    init heading + disco + qcc + ops

---
+++ 04_choose/krewes.py
@@ -0,0 +1,12 @@
+"""
+Kevin Wang and Samson Wu
+SoftDev
+K04 -- Using dictionaries and random to extract a random devo from a dictionary of krewes
+2022-09-22
+time spent: 
+
+DISCO:
+QCC:
+OPS SUMMARY:
+1. 
+"""
