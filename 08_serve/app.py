f1350f16bc0fb5b984c513e491c046902b860158
Kevin Wang <kwang30@stuy.edu>
2022-10-07 13:45:45 -0400

    / route returns TNPG, random occupation, and list of occupations

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -9,7 +9,18 @@ app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def random_occupation():
-    return occupation.get_random_occupation()
+    return_value = "TEAM FLASKERS - Harry ZHu, Kevin Wang, Daniel He </br>"
+    occupation_to_percent, total_percent = occupation.csv_to_dictionary()
+
+    return_value += f"<h1> {occupation.get_random_occupation()} </h1></br>"
+
+    occupations = list(occupation_to_percent.keys())
+
+    for oc in occupations:
+        return_value += oc + "</br>"
+
+    return return_value
+    
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
