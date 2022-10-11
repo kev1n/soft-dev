cc6a66021c9ea376802fec98c0c8a67c4079afbf
Kevin Wang <kwang30@stuy.edu>
2022-10-11 13:54:00 -0400

    comments

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -9,18 +9,20 @@ Disco:
 * f strings to insert the result of python code into a string
 * how to use modules (using github guide)
 * flask must have one return value so use a string with HTML to parse multiple outputs
-
+* br is a self closing tag
+* flask automatically inserts the returned value into the body
 
 QCC:
 * __x__ is some special syntax in markdown
 * how is css handled in flask?
 * why can't multiple values be returned in one function in flask?
-
+* how do we get flask to return a value in the <head></head> tag?
 
 HOW THIS SCRIPT WORKS:
 1. Imports flask and occupation, where occupation.py represents the functions for lab 07
 2. Creates a flask app
-3. In the global scope, parses out the list of occupations from the dictionary of occupations to percentages and adds it to a string, string_of_occupations
+3. In the global scope, parses out the list of occupations from the dictionary of occupations to percentages 
+and adds it to a string, string_of_occupations. The list of occupations never changes granted the occupations stays the same.
 4. In the home route of flask, define the function random_occupation, which returns a string consisting of the TNPG + roster + results of generating a random occupation + list of occupations
 
 """
@@ -31,19 +33,21 @@ import occupation #this is a copy of 06_py_csv
 app = Flask(__name__) #create instance of class Flask
 
 string_of_occupations = ""
-occupation_to_percent, total_percent = occupation.csv_to_dictionary() #move outside of the function because it is static
-occupations = list(occupation_to_percent.keys())
+#move outside of the function because the occupations are static
+occupation_to_percent, total_percent = occupation.csv_to_dictionary() 
+occupations = list(occupation_to_percent.keys()) #list of occupations
 for oc in occupations:
-    string_of_occupations += oc + "</br>"
+    string_of_occupations += oc + "<br>" #<br> is line break
 
 
 @app.route("/")       #assign fxn to route
 def random_occupation():
-    return_value = "TEAM FLASKERS - Harry Zhu, Kevin Wang, Daniel He </br>"
 
-    return_value += f"<h1> {occupation.get_random_occupation()} </h1></br>"
+    return_value = "TEAM FLASKERS - Harry Zhu, Kevin Wang, Daniel He <br><head><title>hello</title></head>" #TNPG + roster
 
-    return return_value + string_of_occupations
+    return_value += f"<h1> {occupation.get_random_occupation()} </h1><br>" #random dynamic occupation
+
+    return return_value + string_of_occupations # list of strings at the end 
     
 
 if __name__ == "__main__":  # true if this file NOT imported
