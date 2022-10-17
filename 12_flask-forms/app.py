7f7b39cbeced4b294e628c195c3e9541213d6ae9
Kevin Wang <kwang30@stuy.edu>
2022-10-17 14:05:35 -0400

    done

---
+++ 12_flask-forms/app.py
@@ -0,0 +1,60 @@
+"""
+Team Foo Bar - Kevin Wang and Wan Ying Li
+SoftDev
+K12 -- establishing how to extract data from POST and GET, and how they work
+2022-10-17
+time spent: 0.8
+"""
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    """
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    """
+    if request.method == "GET":
+        return render_template( 'login.html' )
+    elif request.method == "POST":
+        username = request.form.get("username")
+        return render_template("response.html", username = username, request_method = request.method)
+
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return "Waaaa hooo HAAAH"  #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
