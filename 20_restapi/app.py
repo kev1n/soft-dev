dcf0df0c45af98c92d7f8ee7da2f1ba466f80b54
Kevin Wang <kwang30@stuy.edu>
2022-11-21 14:07:46 -0500

    .

---
+++ 20_restapi/app.py
@@ -0,0 +1,41 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2022
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+'''
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not. 
+TASK: Predict which...
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests.
+Process results.
+
+PROTIP: Insert your own in-line comments
+ wherever they will help
+  your future self and/or current teammates
+   understand what is going on.
+'''
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    
+    return render_template( 'main.html', img_link="/")
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
